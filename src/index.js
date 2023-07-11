795d6981b2537553297eaede6ffe4f9e9fe222d6
alexisklisch <unbosquense@gmail.com>
2023-07-10 23:42:18 -0300

    Add isEven function and mini test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-function isOdd (num, { flex = true }) {
-  if (!flex && isNaN(num)) {
+function isOdd (num, config = { flex: true, float: false }) {
+  if (!config.flex && typeof num !== 'number') {
     throw new Error('The value is not a number')
   }
 
-  if (!num % 2) {
-    throw new Error('The num is not odd')
+  const number = +num
+
+  if (!config.float && number - Math.floor(number) > 0) {
+    throw new Error('The number is a float number. Change the options to enable floating numbers.')
   }
 
+  if (number % 2 === 0) {
+    throw new Error('The num is not odd ⛔️')
+  }
+  console.log(Math.abs(num) + ' is odd ✅')
   return true
 }
 
-export { isOdd }
+function isEven (num, config = { flex: true, float: false }) {
+  if (!config.flex && typeof num !== 'number') {
+    throw new Error('The value is not a number')
+  }
+
+  const number = +num
+
+  if (!config.float && number - Math.floor(number) > 0) {
+    throw new Error('The number is a float number. Change the options to enable floating numbers.')
+  }
+
+  if (number % 2 !== 0) {
+    throw new Error('The num is not even ⛔️')
+  }
+  console.log(Math.abs(num) + ' is even ✅')
+  return true
+}
+
+export { isOdd, isEven }
---
+++ src/test/test.js
@@ -0,0 +1,4 @@
+import { isEven, isOdd } from '../index.js'
+
+isOdd(5)
+isEven(4)
